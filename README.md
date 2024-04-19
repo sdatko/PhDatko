c4d3226c0bca19c017dba379107966a5eb3c1ffc
Szymon Datko <szymon.datko@pwr.edu.pl>
2024-04-19 12:46:06 +0200

    Tox environment to fetch data
    This commit introduces a tox environment with
    necessary script to automate the fetching
    of pre-computed data for Viewer application.

--- README.md
+++ README.md
@@ -16,3 +16,19 @@ Alternatively, this can be achieved conveniently with the `tox` command:
 ```
 tox -e streamlit
 ```
+
+
+Data for the Viewer
+-------------------
+
+The Viewer application relies on the results of the scripts that are located
+in the `research/data/` subdirectory. However, it may take up to several days
+to generate everything from scratch. Hence, for convenience, the pre-computed
+data for the Viewer application can be downloaded using the following command:
+
+```
+tox -e data
+```
+
+It runs the `fetch-precomputed-data.py` script from `research/data/` directory.
+This needs to be performed usually only once.
---
+++ research/data/fetch-precomputed-data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from os.path import exists
+from urllib.request import urlretrieve
+
+REMOTE_BASE_URL = 'https://datko.pl/PhDatko/data/'
+FILES_TO_GET = (
+    'correlations.pickle',
+    'correlations.sqlite',
+    'distributions.pickle',
+    'distributions.sqlite',
+    'overlapping.pickle',
+    'overlapping.sqlite',
+    'properties.pickle',
+    'properties.sqlite',
+)
+
+print('Fetching files:')
+for file in FILES_TO_GET:
+    print(f'-> {file}', end=' ')
+    if exists(file):
+        print('[exists]')
+    else:
+        urlretrieve(f'{REMOTE_BASE_URL}/{file}', f'{file}')
+        print('[ok]')
+
+print('Done!')
