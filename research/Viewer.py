4aaedb77648ba98dd6eefe00db58affb196bca55
Szymon Datko <szymon.datko@pwr.edu.pl>
2024-04-30 00:27:15 +0200

    Update text on the main page

--- research/Viewer.py
+++ research/Viewer.py
@@ -9,6 +9,23 @@ st.set_page_config(
 
 st.title('Hello World!')
 
+st.info('Select a tool from the sidebar menu on the left.')
+
+st.markdown('''
+    - **Viewer** – leads to the main page and this description ;-)
+    - **📊 Histograms** – allows to analyse the distributions of measures
+                          and the separability of the data.
+    - **📈 Trends** – makes it easier to determine the characteristics
+                      and stability of measures as a function
+                      of input parameters.
+    - **🖼️ Measures-illustrated** – contains figures depicting the outlierness
+                                    measures and their properties.
+    - **🗂 DataFrame-browser** – offers a filtered view of the data
+                                 in the form of a table.
+''')
+
+st.markdown('# ')  # Spacing hack
+
 with st.expander('Clearing the cache'):
     st.warning('If the file containing the data has changed after starting'
                ' the Streamlit application, please click on the button below.',
